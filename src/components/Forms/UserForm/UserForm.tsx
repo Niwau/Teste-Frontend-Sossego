8cef1d7ffc2410cb41b9285d5d276834321e7898
Guilherme Ribeiro <xxguilhermesoaresxx@gmail.com>
2022-12-24 11:32:22 -0300

    first commit

---
+++ src/components/Forms/UserForm/UserForm.tsx
@@ -0,0 +1,9 @@
+import { Box, Heading } from '@chakra-ui/react'
+
+export const UserForm = () => {
+  return (
+    <Box>
+      <Heading fontSize={'3.6rem'}>Criação de usuário</Heading>
+    </Box>
+  )
+}
